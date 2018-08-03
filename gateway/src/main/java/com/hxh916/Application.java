dd99a0466a936b4e13a9e9378af192c224b5c9d2
feiwei <flashwarrior@163.com>
2018-08-03 09:34:25 +0800

    info:第一次提交

---
+++ gateway/src/main/java/com/hxh916/Application.java
@@ -0,0 +1,20 @@
+package com.hxh916;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Description:
+ * @Author:
+ * @Date: Created in 18:37 2018/8/2
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
